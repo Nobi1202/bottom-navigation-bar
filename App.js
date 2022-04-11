5285cf23a77e0554076527b376af027447769b5f
Nobi1202 <saobang001111@gmail.com>
2022-04-11 16:04:03 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import {View, Text} from 'react-native';
+
+import MainContainer from './navigation/main_container';
+
+function App() {
+  return <MainContainer></MainContainer>;
+}
+
+export default App;
